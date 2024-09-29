37f5e1c01b441ddd93a85d285c44308fa98a2e74
Imre Bartis <imbartis@gmail.com>
2024-09-29 03:26:02 +0300

    tehtävä 10.7.

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 import theme from '../../theme';
@@ -20,10 +20,12 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab to='/'
-        text='Repositories' />
-      <AppBarTab to='/signin'
-        text='Sign In' />
+      <ScrollView horizontal>
+        <AppBarTab to='/'
+          text='Repositories' />
+        <AppBarTab to='/signin'
+          text='Sign In' />
+      </ScrollView>
     </View>
   );
 };
