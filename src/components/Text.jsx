a25ab35f63d279c8685ff3fe9dfa886369e672cf
Imre Bartis <imbartis@gmail.com>
2024-09-30 16:20:31 +0300

    tehtävä 10.10.

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,11 +1,15 @@
-import { Text as NativeText, StyleSheet } from 'react-native';
+import { Text as NativeText, StyleSheet, Platform } from 'react-native';
 import theme from '../theme';
 
 const styles = StyleSheet.create({
   text: {
     color: theme.colors.textPrimary,
     fontSize: theme.fontSizes.body,
-    fontFamily: theme.fonts.main,
+    fontFamily: Platform.select({
+      ios: 'Arial',
+      android: 'Roboto',
+      default: 'System'
+    }),
     fontWeight: theme.fontWeights.normal
   },
   colorTextSecondary: {
